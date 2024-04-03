cd6dd506dd094cfcb693d8e2c9c23e39f1f595ae
hieuhuu <hieunhhe151327@fpt.edu.vn>
2024-04-03 22:40:39 +0700

    add MapDTO, Mapping profile

---
+++ SEP490_G41/BusinessObject/MappingProfile/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BusinessObject.DTO;
+using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.MappingProfile
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            CreateMap<Map, MapDTO>()
+               .ForMember(dest => dest.FloorName, opt => opt.MapFrom(src => src.Floor.FloorName));
+            CreateMap<MapDTO, Map>();
+
+            // Định nghĩa ánh xạ từ MapAddDTO sang Map và ngược lại
+            CreateMap<MapAddDTO, Map>();
+            CreateMap<Map, MapAddDTO>();
+
+            // Định nghĩa ánh xạ từ MapUpdateDTO sang Map và ngược lại
+            CreateMap<MapUpdateDTO, Map>();
+            CreateMap<Map, MapUpdateDTO>();
+
+        }
+    }
+}
