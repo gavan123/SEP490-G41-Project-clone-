2c5b9841daa1ee9a28cb2b26b0b3d86f69a4df08
minh <minhpche153232@fpt.edu.vn>
2024-04-03 22:50:13 +0700

    Add gitignore

--- SEP490_G41/BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
+++ SEP490_G41/BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788895c3f0a55cd353a8137217a1fd848842d76e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
