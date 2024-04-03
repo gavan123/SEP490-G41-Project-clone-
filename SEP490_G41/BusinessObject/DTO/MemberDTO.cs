7a95d9a566cb81ff7899abcc434cbbd96ca56992
Nguyen Van Tuyen <tuyennvhe150819@fpt.edu.vn>
2024-04-03 21:06:36 +0700

    view_update_profile

---
+++ SEP490_G41/BusinessObject/DTO/MemberDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTO
+{
+    public class MemberDTO
+    {
+        public int MemberId { get; set; }
+        public string FullName { get; set; } = null!;
+        public DateOnly DoB { get; set; }
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public int RoleId { get; set; }
+    }
+    public class MemberUpdateDTO
+    {   
+        public string FullName { get; set; } = null!;
+        public DateOnly DoB { get; set; }
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Status { get; set; } = null!;
+ 
+    }
+}
