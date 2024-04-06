3cc4926358563853a9fbba83465c34a3e6c5d71f
anhnh <anhnhhe163247@fpt.edu.vn>
2024-04-06 18:20:26 +0700

    .

--- SEP490_G41/AR_NavigationAPI/obj/Debug/net6.0/AR_NavigationAPI.AssemblyInfo.cs
+++ SEP490_G41/AR_NavigationAPI/obj/Debug/net6.0/AR_NavigationAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AR_NavigationAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d597c09b87041be106e625dab3277397807a667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9c7a1ba186c409c51d09dc9b13189cd7130b67")]
 [assembly: System.Reflection.AssemblyProductAttribute("AR_NavigationAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AR_NavigationAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
