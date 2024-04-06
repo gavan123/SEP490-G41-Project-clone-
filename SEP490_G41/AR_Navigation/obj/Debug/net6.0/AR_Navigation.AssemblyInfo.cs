c318e39e154d436d6e9640728ba73150aafc0072
anhnh <anhnhhe163247@fpt.edu.vn>
2024-04-06 18:45:35 +0700

    Giải quyết xung đột hợp nhất

--- SEP490_G41/AR_Navigation/obj/Debug/net6.0/AR_Navigation.AssemblyInfo.cs
+++ SEP490_G41/AR_Navigation/obj/Debug/net6.0/AR_Navigation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AR_Navigation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56408b012994d038891613407344ab4a1d570de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f69bd23eac9d65d9ee10a73d7c6f5e403548f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AR_Navigation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AR_Navigation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
