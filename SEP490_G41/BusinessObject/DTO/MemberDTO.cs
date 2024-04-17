f475c90cc7eed47945def12b477271734bd76ac4
Nguyen Van Tuyen <tuyennvhe150819@fpt.edu.vn>
2024-04-17 20:07:32 +0700

    fix edit profile

--- SEP490_G41/BusinessObject/DTO/MemberDTO.cs
+++ SEP490_G41/BusinessObject/DTO/MemberDTO.cs
@@ -38,7 +38,7 @@ namespace BusinessObject.DTO
     {
         public int MemberId { get; set; }
         public string FullName { get; set; } = null!;
-        public DateTime DoB { get; set; }
+        public DateTime DoB { get; set; } 
         public string Address { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public string Email { get; set; } = null!;
