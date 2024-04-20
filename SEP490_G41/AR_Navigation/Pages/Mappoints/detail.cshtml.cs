c57f8640cbc5822aa076e6f2f43a2944d9fe48b2
hieuhuu <hieunhhe151327@fpt.edu.vn>
2024-04-20 15:15:59 +0700

    add mappoint detail

---
+++ SEP490_G41/AR_Navigation/Pages/Mappoints/detail.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AR_Navigation.Pages.Mappoints
+{
+    public class detailModel : PageModel
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<listModel> _logger;
+
+
+        public detailModel(IWebHostEnvironment webHostEnvironment, ILogger<listModel> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+        [BindProperty]
+        public int FloorId { get; set; }
+        [BindProperty]
+        public IFormFile ImageFile { get; set; }
+        public async Task<IActionResult> OnPostAddOrEditMapAsync()
+        {
+
+            string imagesDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+
+            string uniqueFileName = null;
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                uniqueFileName = ImageFile.FileName;
+
+                string filePath = Path.Combine(imagesDirectory, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+            }
+            _logger.LogInformation($"Images directory: {imagesDirectory}");
+
+
+            if (FloorId != 0)
+            {
+                return RedirectToPage("/Building/detail/" + FloorId);
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+    }
+}
