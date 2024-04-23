0ce53e7a01abcc5fa6bcb2e4b829ade4a426ce09
hieuhuu <hieunhhe151327@fpt.edu.vn>
2024-04-24 02:16:08 +0700

    update

--- SEP490_G41/AR_Navigation/Pages/Mappoints/detail.cshtml.cs
+++ SEP490_G41/AR_Navigation/Pages/Mappoints/detail.cshtml.cs
@@ -5,7 +5,64 @@ namespace AR_Navigation.Pages.Mappoints
 {
     public class detailModel : PageModel
     {
-        
 
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<listModel> _logger;
+
+
+        public detailModel(IWebHostEnvironment webHostEnvironment, ILogger<listModel> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+
+        [BindProperty]
+        public IFormFile ImageFile { get; set; }
+        public async Task<IActionResult> OnPostCreateBuildingAsync()
+        {
+
+            string imagesDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "Images/Mappoint");
+
+            string uniqueFileName = null;
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                uniqueFileName = ImageFile.FileName;
+
+                string filePath = Path.Combine(imagesDirectory, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+            }
+            _logger.LogInformation($"Images directory: {imagesDirectory}");
+
+
+            return RedirectToPage("/Maps/list");
+        }
+
+        public async Task<IActionResult> OnPostEditBuildingAsync()
+        {
+
+            string imagesDirectory = Path.Combine(_webHostEnvironment.WebRootPath, "Images/Mappoint");
+
+            string uniqueFileName = null;
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                uniqueFileName = ImageFile.FileName;
+
+                string filePath = Path.Combine(imagesDirectory, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+            }
+            _logger.LogInformation($"Images directory: {imagesDirectory}");
+
+
+            return RedirectToPage("/Maps/list");
+        }
     }
 }
